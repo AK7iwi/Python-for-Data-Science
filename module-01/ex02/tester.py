c358c04b599a47e28328bffb5ebd558695b8e899
AK7iwi <atzlts@proton.me>
2025-10-03 10:15:44 +0200

    modoule01/ex00: test renaming

--- module-01/ex02/tester.py
+++ module-01/ex02/tester.py
@@ -1,7 +1,14 @@
 from load_image import ft_load
 
+# Test 1a: Basic valid input
+print("Test 1a: Basic valid input")
+
 try:
     print(ft_load("landscape.jpg"))
     
 except Exception as e:
     print(f"Error: {e}")
+
+print("--------------------------------")
+
+# Test 1b: 
